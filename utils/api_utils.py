84e992a2d8dc71c4b938fcd12a6bb8f3c5c485c3
moises-herrera <moisesherrera037@gmail.com>
2025-10-19 10:45:54 -0500

    refactor: move utility methods

---
+++ utils/api_utils.py
@@ -0,0 +1,60 @@
+from flask import request
+
+
+def parse_filters(query_params):
+    query_params = request.args
+
+    validate_filters(query_params)
+
+    age = query_params.get("age")
+    gender = query_params.get("gender")
+    heart_disease = query_params.get("heart_disease")
+
+    filters = {}
+
+    if age:
+        filters["Age"] = int(age)
+    if gender:
+        filters["Sex"] = gender
+    if heart_disease:
+        filters["HeartDisease"] = int(heart_disease)
+
+    return filters
+
+
+def validate_filters(filters):
+    valid_keys = {"Age", "Sex", "HeartDisease"}
+    for key in filters.keys():
+        if key not in valid_keys:
+            raise ValueError(
+                {
+                    "error": f"Filtro inválido: {key}. Los filtros válidos son: {', '.join(valid_keys)}"
+                },
+                400,
+            )
+
+
+def validate_patient_data(data):
+    required_fields = {
+        "Age",
+        "Sex",
+        "ChestPainType",
+        "RestingBP",
+        "Cholesterol",
+        "FastingBS",
+        "RestingECG",
+        "MaxHR",
+        "ExerciseAngina",
+        "Oldpeak",
+        "ST_Slope",
+    }
+    missing_fields = []
+
+    for field in required_fields:
+        if field not in data:
+            missing_fields.append(field)
+
+    if missing_fields:
+        raise ValueError(
+            {"error": f"Faltan campos obligatorios: {', '.join(missing_fields)}"}, 400
+        )
